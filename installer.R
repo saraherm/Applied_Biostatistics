5aa26c63049478053a3dd8d28fafd02c0b3ae7ae
Sara Hermann <slh@msu.edu>
2022-04-13 16:03:48 -0400

    Add files via upload

---
+++ installer.R
@@ -0,0 +1,8 @@
+#######################
+###Install.packages ###
+#######################
+install.packages('learnr')
+install.packages('reshape2')
+install.packages('ggplot2')
+install.packages('patchwork')
+install.packages('knitr')
